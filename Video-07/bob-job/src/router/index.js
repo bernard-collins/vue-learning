31e5848c06cb6dc98969713d25551bf8680ae4fa
bernard-collins <bernard.collins@bookingonline.co.uk>
2021-11-09 10:19:30 +0000

    feat: beginning lesson 7

--- Video-07/bob-job/src/router/index.js
+++ Video-07/bob-job/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
 
 const routes = [
   {
@@ -9,11 +10,13 @@ const routes = [
   },
   {
     path: '/about',
+    // path: '/about-us',
     name: 'About',
+    component: About
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    //component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
--- Video-07/bob-job/src/views/Home.vue
+++ Video-07/bob-job/src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+   <div class="home">
+      <h1>Homepage</h1>
+      <p>
+         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus exercitationem, nesciunt accusantium fugiat veniam totam corrupti quibusdam ea fuga adipisci magni dolor reprehenderit
+         praesentium eveniet cumque eius, maxime beatae iste. Libero modi delectus atque minima velit dolorem eaque consequatur, suscipit perspiciatis illum quia aliquid, natus ipsam consectetur, sunt
+         sequi! Aperiam incidunt alias repudiandae saepe quo hic doloribus maxime ipsa quisquam voluptatibus. Nam officiis inventore, neque doloremque magnam perferendis amet expedita voluptate vitae
+         temporibus ab officia, obcaecati nesciunt! In provident quaerat laboriosam suscipit laudantium quos accusamus totam reiciendis, inventore nemo sapiente vel corrupti adipisci ducimus commodi
+         error architecto, optio quam. Distinctio.
+      </p>
+      <p>
+         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus exercitationem, nesciunt accusantium fugiat veniam totam corrupti quibusdam ea fuga adipisci magni dolor reprehenderit
+         praesentium eveniet cumque eius, maxime beatae iste. Libero modi delectus atque minima velit dolorem eaque consequatur, suscipit perspiciatis illum quia aliquid, natus ipsam consectetur, sunt
+         sequi! Aperiam incidunt alias repudiandae saepe quo hic doloribus maxime ipsa quisquam voluptatibus. Nam officiis inventore, neque doloremque magnam perferendis amet expedita voluptate vitae
+         temporibus ab officia, obcaecati nesciunt! In provident quaerat laboriosam suscipit laudantium quos accusamus totam reiciendis, inventore nemo sapiente vel corrupti adipisci ducimus commodi
+         error architecto, optio quam. Distinctio.
+      </p>
+   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+   name: 'Home',
+   components: {}
+};
 </script>
