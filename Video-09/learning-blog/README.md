68d7a959c7b5d917f899dd01d234ba1a574e2017
bernard-collins <bernard.collins@bookingonline.co.uk>
2021-11-09 14:59:54 +0000

    feat: Halfway through video 9

--- Video-09/learning-blog/README.md
+++ Video-09/learning-blog/README.md
@@ -1,19 +1,33 @@
+# Video 9
+
+-  Drawbacks of the Options API
+-  Composition API basics
+-  Template refs (again)
+-  Using refs for reactivity
+-  Refs vs Reactive
+-  Computed values
+-  watch & watchEffect
+
 # learning-blog
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- Video-09/learning-blog/src/views/Home.vue
+++ Video-09/learning-blog/src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+   <div class="home">
+      Home
+      <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
+      <button @click="handleClick">click me</button>
+      <button @click="age++">add 1 to age</button>
+      <input type="text" v-model="name" />
+   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from 'vue';
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+   name: 'Home',
+   setup() {
+      // console.log('setup');
+      // "this.refs" can't be used in setup
+      // const p = ref(null);
+
+      const name = ref('Gary King');
+      const age = ref(96);
+
+      const handleClick = () => {
+         name.value = 'Shaun Riley';
+         age.value = 21;
+
+         //  console.log(p, p.value);
+         //  p.value.classList.add('test');
+         //  p.value.textContent = 'Good Mornin Yall';
+      };
+
+      return { name, age, handleClick };
+   }
+   // setup runs before any other lifecycle hook
+   // created() {
+   //     console.log('created');
+   //  },
+   //  mounted() {
+   //     console.log('mounted');
+   //  }
+};
 </script>
