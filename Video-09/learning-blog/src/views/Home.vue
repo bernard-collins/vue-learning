9baf349bcb1daae505530bb278de77b921c977eb
bernard-collins <bernard.collins@bookingonline.co.uk>
2021-11-09 17:34:54 +0000

    feat: Finishing video 9

--- Video-09/learning-blog/src/views/Home.vue
+++ Video-09/learning-blog/src/views/Home.vue
@@ -1,12 +1,19 @@
 <template>
    <div class="home">
       <h1>Home</h1>
-      <h2>Refs</h2>
+      <input type="text" v-model="search" />
+      <p>search term - {{ search }}</p>
+      <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+      <button @click="handleClick">stop watching</button>
+
+      <!-- <p>{{ name }}</p> -->
+
+      <!-- <h2>Refs</h2>
       <p>{{ charOne.name }} - {{ charOne.age }}</p>
       <button @click="updateCharOne">Update character one</button>
       <h2>Reactive</h2>
       <p>{{ charTwo.name }} - {{ charTwo.age }}</p>
-      <button @click="updateCharTwo">Update character two</button>
+      <button @click="updateCharTwo">Update character two</button> -->
 
       <!-- Home
       <p ref="p">My name is {{ name }} and my age is {{ age }}</p>
@@ -17,40 +24,61 @@
 </template>
 
 <script>
-import { ref, reactive } from 'vue';
+// import { ref, reactive } from 'vue';
+import { computed, ref, watch, watchEffect } from 'vue';
 
 export default {
    name: 'Home',
    setup() {
-      const charOne = ref({ name: 'Dom', age: 66 });
-      const charTwo = reactive({ name: 'Lee', age: 19 });
+      const search = ref('');
+      const names = ref(['Dom', 'Lee', 'Clay', 'Lambert', 'Wilfred', 'Warren']);
 
-      const updateCharOne = () => {
-         charOne.value.age = 71;
+      const stopWatch = watch(search, () => {
+         console.log('watch function ran');
+      });
+
+      const stopEffect = watchEffect(() => {
+         console.log('watchEffect function ran', search.value);
+      });
+
+      const matchingNames = computed(() => {
+         return names.value.filter((name) => name.includes(search.value));
+      });
+
+      const handleClick = () => {
+         stopWatch();
+         stopEffect();
       };
 
-      const updateCharTwo = () => {
-         charTwo.age = 20;
-      };
+      return { names, search, matchingNames, handleClick };
 
-      return { charOne, updateCharOne, charTwo, updateCharTwo };
+      // const name = computed(() => {
+      //    return 'Shaun';
+      // });
 
+      // return { name };
+
+      // const charOne = ref({ name: 'Dom', age: 66 });
+      // const charTwo = reactive({ name: 'Lee', age: 19 });
+      // const updateCharOne = () => {
+      //    charOne.value.age = 71;
+      // };
+      // const updateCharTwo = () => {
+      //    charTwo.age = 20;
+      // };
+      // return { charOne, updateCharOne, charTwo, updateCharTwo };
       // console.log('setup');
       // "this.refs" can't be used in setup
       // const p = ref(null);
-
       // const name = ref('Gary King');
       // const age = ref(96);
-
       // const handleClick = () => {
       //    name.value = 'Shaun Riley';
       //    age.value = 21;
-
       //   console.log(p, p.value);
       //  p.value.classList.add('test');
       //  p.value.textContent = 'Good Mornin Yall';
       // };
-
       //  return { name, age, handleClick };
    }
    // setup runs before any other lifecycle hook
