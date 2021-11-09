b7cd52a4d33cd05bbcde6d8e64aa74f728057f0f
bernard-collins <bernard.collins@bookingonline.co.uk>
2021-11-09 12:28:05 +0000

    feat: finished video 8

--- Video-08/bob-job/src/views/Jobs/Jobs.vue
+++ Video-08/bob-job/src/views/Jobs/Jobs.vue
@@ -1,11 +1,16 @@
 <template>
    <h1>Jobs</h1>
-   <dev v-for="job in jobs" :key="job.id" class="job">
-      <!-- <h2>{{ job.title }}</h2> -->
-      <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
-         <h2>{{ job.title }}</h2>
-      </router-link>
-   </dev>
+   <div v-if="jobs.length">
+      <div v-for="job in jobs" :key="job.id" class="job">
+         <!-- <h2>{{ job.title }}</h2> -->
+         <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
+            <h2>{{ job.title }}</h2>
+         </router-link>
+      </div>
+   </div>
+   <div v-else>
+      <p>Loading Job details...</p>
+   </div>
 </template>
 
 <script>
@@ -13,11 +18,17 @@ export default {
    data() {
       return {
          jobs: [
-            { title: 'Bobs Odd Jobs', id: 1, details: 'Pizza Man Extraudinair' },
-            { title: 'Jobs Bobs Odd', id: 2, details: 'Extraudinair Pizza Man' },
-            { title: 'Odd Jobs Bobs', id: 3, details: 'Man Extraudinair Pizza' }
+            // { title: 'Bobs Odd Jobs', id: 1, details: 'Pizza Man Extraordinaire' },
+            // { title: 'Jobs Bobs Odd', id: 2, details: 'Extraordinaire Pizza Man' },
+            // { title: 'Odd Jobs Bobs', id: 3, details: 'Man Extraordinaire Pizza' }
          ]
       };
+   },
+   mounted() {
+      fetch('http://localhost:3000/jobs')
+         .then((res) => res.json())
+         .then((data) => (this.jobs = data))
+         .catch((err) => console.log(err.message));
    }
 };
 </script>
